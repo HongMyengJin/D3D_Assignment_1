c7f6f380a49c6d0789d4d65ffdc985888c335f01
Hong Myeng Jin <mongjinjin@tukorea.ac.kr>
2022-12-04 15:14:10 +0900

    Particle Shader 추가

--- Shaders.hlsl
+++ Shaders.hlsl
@@ -1,10 +1,3 @@
-#define Texture_Color 0
-#define Texture_Texture 2
-#define Texture_Normal 2
-#define Texture_Illumination 3
-#define Texture_ObjectID_zDepth 4
-#define Texture_NormalV 5
-#define Texture_Depth 6
 struct MATERIAL
 {
 	float4					m_cAmbient;
@@ -16,8 +9,12 @@ struct MATERIAL
 
 cbuffer cbFrameworkInfo : register(b0)
 {
-	float 		gfCurrentTime;
-	float		gfElapsedTime;
+	float		gfCurrentTime : packoffset(c0.x);
+	float		gfElapsedTime : packoffset(c0.y);
+	float		gfSecondsPerFirework : packoffset(c0.z);
+	int			gnFlareParticlesToEmit : packoffset(c0.w);;
+	float3		gf3Gravity : packoffset(c1.x);
+	int			gnMaxFlareType2Particles : packoffset(c1.w);;
 };
 
 
@@ -27,6 +24,7 @@ cbuffer cbCameraInfo : register(b1)
 	matrix		gmtxProjection : packoffset(c4);
 	float3		gvCameraPosition : packoffset(c8);
 	matrix		gmtxInverseProjection : packoffset(c12);
+	matrix		gmtxInverseView : packoffset(c16);
 };
 
 cbuffer cbGameObjectInfo : register(b2)
@@ -62,21 +60,35 @@ cbuffer cbDrawOptions : register(b5)
 
 //#define _WITH_STANDARD_TEXTURE_MULTIPLE_DESCRIPTORS
 
-#ifdef _WITH_STANDARD_TEXTURE_MULTIPLE_DESCRIPTORS
-Texture2D gtxtAlbedoTexture : register(t6);
-Texture2D gtxtSpecularTexture : register(t7);
-Texture2D gtxtNormalTexture : register(t8);
-Texture2D gtxtMetallicTexture : register(t9);
-Texture2D gtxtEmissionTexture : register(t10);
-Texture2D gtxtDetailAlbedoTexture : register(t11);
-Texture2D gtxtDetailNormalTexture : register(t12);
-#else
-Texture2D gtxtStandardTextures[7] : register(t6);
-#endif
+//#ifdef _WITH_STANDARD_TEXTURE_MULTIPLE_DESCRIPTORS
+//Texture2D gtxtAlbedoTexture : register(t6);
+//Texture2D gtxtSpecularTexture : register(t7);
+//Texture2D gtxtNormalTexture : register(t8);
+//Texture2D gtxtMetallicTexture : register(t9);
+//Texture2D gtxtEmissionTexture : register(t10);
+//Texture2D gtxtDetailAlbedoTexture : register(t11);
+//Texture2D gtxtDetailNormalTexture : register(t12);
+//#else
 
-SamplerState gssWrap : register(s0);
+//#endif
 
 Texture2D gtxtTexture : register(t0);
+
+// ---------------------파티클--------------------
+Texture2D<float4> gtxtParticleTexture : register(t1);
+//Texture1D<float4> gtxtRandom : register(t2);
+Buffer<float4> gRandomBuffer : register(t2);
+Buffer<float4> gRandomSphereBuffer : register(t3);
+// -----------------------------------------------
+
+Texture2D gtxtStandardTextures[7] : register(t6);
+
+SamplerState gWrapSamplerState : register(s0);
+SamplerState gClampSamplerState : register(s1);
+SamplerState gMirrorSamplerState : register(s2);
+SamplerState gPointSamplerState : register(s3);
+
+
 struct VS_STANDARD_INPUT
 {
 	float3 position : POSITION;
@@ -119,17 +131,17 @@ float4 PSStandard(VS_STANDARD_OUTPUT input) : SV_TARGET
 	float4 cEmissionColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
 
 #ifdef _WITH_STANDARD_TEXTURE_MULTIPLE_DESCRIPTORS
-	if (gnTexturesMask & MATERIAL_ALBEDO_MAP) cAlbedoColor = gtxtAlbedoTexture.Sample(gssWrap, input.uv);
-	if (gnTexturesMask & MATERIAL_SPECULAR_MAP) cSpecularColor = gtxtSpecularTexture.Sample(gssWrap, input.uv);
-	if (gnTexturesMask & MATERIAL_NORMAL_MAP) cNormalColor = gtxtNormalTexture.Sample(gssWrap, input.uv);
-	if (gnTexturesMask & MATERIAL_METALLIC_MAP) cMetallicColor = gtxtMetallicTexture.Sample(gssWrap, input.uv);
-	if (gnTexturesMask & MATERIAL_EMISSION_MAP) cEmissionColor = gtxtEmissionTexture.Sample(gssWrap, input.uv);
+	if (gnTexturesMask & MATERIAL_ALBEDO_MAP) cAlbedoColor = gtxtAlbedoTexture.Sample(gWrapSamplerState, input.uv);
+	if (gnTexturesMask & MATERIAL_SPECULAR_MAP) cSpecularColor = gtxtSpecularTexture.Sample(gWrapSamplerState, input.uv);
+	if (gnTexturesMask & MATERIAL_NORMAL_MAP) cNormalColor = gtxtNormalTexture.Sample(gWrapSamplerState, input.uv);
+	if (gnTexturesMask & MATERIAL_METALLIC_MAP) cMetallicColor = gtxtMetallicTexture.Sample(gWrapSamplerState, input.uv);
+	if (gnTexturesMask & MATERIAL_EMISSION_MAP) cEmissionColor = gtxtEmissionTexture.Sample(gWrapSamplerState, input.uv);
 #else
-	if (gnTexturesMask & MATERIAL_ALBEDO_MAP) cAlbedoColor = gtxtStandardTextures[0].Sample(gssWrap, input.uv);
-	if (gnTexturesMask & MATERIAL_SPECULAR_MAP) cSpecularColor = gtxtStandardTextures[1].Sample(gssWrap, input.uv);
-	if (gnTexturesMask & MATERIAL_NORMAL_MAP) cNormalColor = gtxtStandardTextures[2].Sample(gssWrap, input.uv);
-	if (gnTexturesMask & MATERIAL_METALLIC_MAP) cMetallicColor = gtxtStandardTextures[3].Sample(gssWrap, input.uv);
-	if (gnTexturesMask & MATERIAL_EMISSION_MAP) cEmissionColor = gtxtStandardTextures[4].Sample(gssWrap, input.uv);
+	if (gnTexturesMask & MATERIAL_ALBEDO_MAP) cAlbedoColor = gtxtStandardTextures[0].Sample(gWrapSamplerState, input.uv);
+	if (gnTexturesMask & MATERIAL_SPECULAR_MAP) cSpecularColor = gtxtStandardTextures[1].Sample(gWrapSamplerState, input.uv);
+	if (gnTexturesMask & MATERIAL_NORMAL_MAP) cNormalColor = gtxtStandardTextures[2].Sample(gWrapSamplerState, input.uv);
+	if (gnTexturesMask & MATERIAL_METALLIC_MAP) cMetallicColor = gtxtStandardTextures[3].Sample(gWrapSamplerState, input.uv);
+	if (gnTexturesMask & MATERIAL_EMISSION_MAP) cEmissionColor = gtxtStandardTextures[4].Sample(gWrapSamplerState, input.uv);
 #endif
 
 	float4 cIllumination = float4(1.0f, 1.0f, 1.0f, 1.0f);
@@ -210,7 +222,7 @@ VS_SPRITE_TEXTURED_OUTPUT VSTextured(VS_SPRITE_TEXTURED_INPUT input)
 
 float4 PSTextured(VS_SPRITE_TEXTURED_OUTPUT input) : SV_TARGET
 {
-	float4 cColor = gtxtTexture.Sample(gssWrap, input.uv);
+	float4 cColor = gtxtTexture.Sample(gWrapSamplerState, input.uv);
 
 	return(cColor);
 }
@@ -224,6 +236,9 @@ VS_SPRITE_TEXTURED_OUTPUT VSSpriteAnimation(VS_SPRITE_TEXTURED_INPUT input)
 	//output.uv = input.uv;
 	return(output);
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
 
 /*
 float4 PSTextured(VS_SPRITE_TEXTURED_OUTPUT input, uint nPrimitiveID : SV_PrimitiveID) : SV_TARGET
@@ -255,7 +270,7 @@ Texture2D gtxtDetailTexture : register(t15);
 
 float4 PSTerrain(VS_SPRITE_TEXTURED_OUTPUT input) : SV_TARGET
 {
-	float4 cColor = gtxtTerrainTexture.Sample(gssWrap, input.uv);
+	float4 cColor = gtxtTerrainTexture.Sample(gWrapSamplerState, input.uv);
 
 	return(cColor);
 }
@@ -290,9 +305,9 @@ VS_TERRAIN_OUTPUT VSTerrain(VS_TERRAIN_INPUT input)
 
 float4 PSTerrain(VS_TERRAIN_OUTPUT input) : SV_TARGET
 {
-	float4 cBaseTexColor = gtxtTerrainTexture.Sample(gssWrap, input.uv0);
-	float4 cDetailTexColor = gtxtDetailTexture.Sample(gssWrap, input.uv1);
-	float fAlpha = gtxtTerrainTexture.Sample(gssWrap, input.uv0);
+	float4 cBaseTexColor = gtxtTerrainTexture.Sample(gWrapSamplerState, input.uv0);
+	float4 cDetailTexColor = gtxtDetailTexture.Sample(gWrapSamplerState, input.uv1);
+	float fAlpha = gtxtTerrainTexture.Sample(gWrapSamplerState, input.uv0);
 
 	//float4 cColor = cBaseTexColor * 0.5f + cDetailTexColor * 0.5f;
 	float4 cColor = saturate(lerp(cBaseTexColor, cDetailTexColor, fAlpha));
@@ -344,9 +359,9 @@ VS_WATER_OUTPUT VSRippleWater(VS_WATER_INPUT input)
 float4 PSRippleWater(VS_WATER_OUTPUT input) : SV_TARGET
 {
 	//	float4 cBaseTexColor = gtxtWaterBaseTexture.Sample(gSamplerState, input.uv0);
-		float4 cBaseTexColor = gtxtWaterBaseTexture.Sample(gssWrap, float2(input.uv0.x, input.uv0.y - abs(sin(gfCurrentTime)) * 0.0151f));
+		float4 cBaseTexColor = gtxtWaterBaseTexture.Sample(gWrapSamplerState, float2(input.uv0.x, input.uv0.y - abs(sin(gfCurrentTime)) * 0.0151f));
 		//	float4 cColor = input.color * 0.3f + cBaseTexColor * 0.7f;
-			float4 cDetailTexColor = gtxtWaterDetailTexture.Sample(gssWrap, input.uv0 * 10.0f);
+			float4 cDetailTexColor = gtxtWaterDetailTexture.Sample(gWrapSamplerState, input.uv0 * 10.0f);
 			float4 cColor = (cBaseTexColor * 0.3f + cDetailTexColor * 0.7f) + float4(0.0f, 0.0f, 0.15f, 0.0f);
 			cColor *= input.color;
 
@@ -507,15 +522,15 @@ float4 Outline(VS_SCREEN_RECT_TEXTURED_OUTPUT input)
 	float2 f2BottomRightUV = input.uv + float2((1.0f / gtxtStandardTextures[0].Length.x) * fHalfScaleCeil, -(1.0f / gtxtStandardTextures[0].Length.y * fHalfScaleFloor));
 	float2 f2TopLeftUV = input.uv + float2(-(1.0f / gtxtStandardTextures[0].Length.x) * fHalfScaleFloor, (1.0f / gtxtStandardTextures[0].Length.y) * fHalfScaleCeil);
 
-	float3 f3NormalV0 = gtxtStandardTextures[6].Sample(gssWrap, f2BottomLeftUV).rgb;
-	float3 f3NormalV1 = gtxtStandardTextures[6].Sample(gssWrap, f2TopRightUV).rgb;
-	float3 f3NormalV2 = gtxtStandardTextures[6].Sample(gssWrap, f2BottomRightUV).rgb;
-	float3 f3NormalV3 = gtxtStandardTextures[6].Sample(gssWrap, f2TopLeftUV).rgb;
+	float3 f3NormalV0 = gtxtStandardTextures[6].Sample(gWrapSamplerState, f2BottomLeftUV).rgb;
+	float3 f3NormalV1 = gtxtStandardTextures[6].Sample(gWrapSamplerState, f2TopRightUV).rgb;
+	float3 f3NormalV2 = gtxtStandardTextures[6].Sample(gWrapSamplerState, f2BottomRightUV).rgb;
+	float3 f3NormalV3 = gtxtStandardTextures[6].Sample(gWrapSamplerState, f2TopLeftUV).rgb;
 
-	float fDepth0 = gtxtStandardTextures[6].Sample(gssWrap, f2BottomLeftUV).b;
-	float fDepth1 = gtxtStandardTextures[6].Sample(gssWrap, f2TopRightUV).b;
-	float fDepth2 = gtxtStandardTextures[6].Sample(gssWrap, f2BottomRightUV).b;
-	float fDepth3 = gtxtStandardTextures[6].Sample(gssWrap, f2TopLeftUV).b;
+	float fDepth0 = gtxtStandardTextures[6].Sample(gWrapSamplerState, f2BottomLeftUV).b;
+	float fDepth1 = gtxtStandardTextures[6].Sample(gWrapSamplerState, f2TopRightUV).b;
+	float fDepth2 = gtxtStandardTextures[6].Sample(gWrapSamplerState, f2BottomRightUV).b;
+	float fDepth3 = gtxtStandardTextures[6].Sample(gWrapSamplerState, f2TopLeftUV).b;
 
 	float3 f3NormalV = f3NormalV0 * 2.0f - 1.0f;
 	float fNdotV = 1.0f - dot(f3NormalV, -input.viewSpaceDir);
@@ -538,7 +553,7 @@ float4 Outline(VS_SCREEN_RECT_TEXTURED_OUTPUT input)
 	float fEdge = max(fDdgeDepth, fEdgeNormal);
 	float4 f4EdgeColor = float4(1.0f, 1.0f, 1.0f, 1.0f * fEdgeNormal);
 
-	float4 f4Color = gtxtStandardTextures[0].Sample(gssWrap, input.uv);
+	float4 f4Color = gtxtStandardTextures[0].Sample(gWrapSamplerState, input.uv);
 
 	return(AlphaBlend(f4EdgeColor, f4Color));
 }
@@ -551,17 +566,17 @@ float4 PSScreenRectSamplingTextured(VS_SCREEN_RECT_TEXTURED_OUTPUT input) : SV_T
 	{
 		case 84: //'T'
 		{
-			cColor = gtxtStandardTextures[2].Sample(gssWrap, input.uv);
+			cColor = gtxtStandardTextures[2].Sample(gWrapSamplerState, input.uv);
 			break;
 		}
 		case 76: //'L'
 		{
-			cColor = gtxtStandardTextures[3].Sample(gssWrap, input.uv);
+			cColor = gtxtStandardTextures[3].Sample(gWrapSamplerState, input.uv);
 			break;
 		}
 		case 78: //'N'
 		{
-			cColor = gtxtStandardTextures[1].Sample(gssWrap, input.uv);
+			cColor = gtxtStandardTextures[1].Sample(gWrapSamplerState, input.uv);
 			break;
 		}
 		case 68: //'D'
@@ -595,7 +610,7 @@ float4 PSScreenRectSamplingTextured(VS_SCREEN_RECT_TEXTURED_OUTPUT input) : SV_T
 		}
 		case 69: //'E'
 		{
-			//cColor = gtxtStandardTextures[6].Sample(gssWrap, input.uv);
+			//cColor = gtxtStandardTextures[6].Sample(gWrapSamplerState, input.uv);
 			cColor = LaplacianEdge(input.position);
 			//cColor = Outline(input);
 			if (cColor.g <= 1.f)
@@ -606,3 +621,263 @@ float4 PSScreenRectSamplingTextured(VS_SCREEN_RECT_TEXTURED_OUTPUT input) : SV_T
 	return(cColor);
 }
 
+
+// --------------------------------------------------파티클-------------------------------
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+#define PARTICLE_TYPE_EMITTER		0
+#define PARTICLE_TYPE_SHELL			1
+#define PARTICLE_TYPE_FLARE01		2
+#define PARTICLE_TYPE_FLARE02		3
+#define PARTICLE_TYPE_FLARE03		4
+
+#define SHELL_PARTICLE_LIFETIME		3.0f
+#define FLARE01_PARTICLE_LIFETIME	2.5f
+#define FLARE02_PARTICLE_LIFETIME	1.5f
+#define FLARE03_PARTICLE_LIFETIME	2.0f
+
+struct VS_PARTICLE_INPUT
+{
+	float3 position : POSITION;
+	float3 velocity : VELOCITY;
+	float lifetime : LIFETIME;
+	uint type : PARTICLETYPE;
+};
+
+VS_PARTICLE_INPUT VSParticleStreamOutput(VS_PARTICLE_INPUT input)
+{
+	return(input);
+}
+
+float3 GetParticleColor(float fAge, float fLifetime)
+{
+	float3 cColor = float3(1.0f, 1.0f, 1.0f);
+
+	if (fAge == 0.0f) cColor = float3(0.0f, 1.0f, 0.0f);
+	else if (fLifetime == 0.0f)
+		cColor = float3(1.0f, 1.0f, 0.0f);
+	else
+	{
+		float t = fAge / fLifetime;
+		cColor = lerp(float3(1.0f, 0.0f, 0.0f), float3(0.0f, 0.0f, 1.0f), t * 1.0f);
+	}
+
+	return(cColor);
+}
+
+void GetBillboardCorners(float3 position, float2 size, out float4 pf4Positions[4])
+{
+	float3 f3Up = float3(0.0f, 1.0f, 0.0f);
+	float3 f3Look = normalize(gvCameraPosition - position);
+	float3 f3Right = normalize(cross(f3Up, f3Look));
+
+	pf4Positions[0] = float4(position + size.x * f3Right - size.y * f3Up, 1.0f);
+	pf4Positions[1] = float4(position + size.x * f3Right + size.y * f3Up, 1.0f);
+	pf4Positions[2] = float4(position - size.x * f3Right - size.y * f3Up, 1.0f);
+	pf4Positions[3] = float4(position - size.x * f3Right + size.y * f3Up, 1.0f);
+}
+
+void GetPositions(float3 position, float2 f2Size, out float3 pf3Positions[8])
+{
+	float3 f3Right = float3(1.0f, 0.0f, 0.0f);
+	float3 f3Up = float3(0.0f, 1.0f, 0.0f);
+	float3 f3Look = float3(0.0f, 0.0f, 1.0f);
+
+	float3 f3Extent = normalize(float3(1.0f, 1.0f, 1.0f));
+
+	pf3Positions[0] = position + float3(-f2Size.x, 0.0f, -f2Size.y);
+	pf3Positions[1] = position + float3(-f2Size.x, 0.0f, +f2Size.y);
+	pf3Positions[2] = position + float3(+f2Size.x, 0.0f, -f2Size.y);
+	pf3Positions[3] = position + float3(+f2Size.x, 0.0f, +f2Size.y);
+	pf3Positions[4] = position + float3(-f2Size.x, 0.0f, 0.0f);
+	pf3Positions[5] = position + float3(+f2Size.x, 0.0f, 0.0f);
+	pf3Positions[6] = position + float3(0.0f, 0.0f, +f2Size.y);
+	pf3Positions[7] = position + float3(0.0f, 0.0f, -f2Size.y);
+}
+
+float4 RandomDirection(float fOffset)
+{
+	int u = uint(gfCurrentTime + fOffset + frac(gfCurrentTime) * 1000.0f) % 1024;
+	return(normalize(gRandomBuffer.Load(u)));
+}
+
+float4 RandomDirectionOnSphere(float fOffset)
+{
+	int u = uint(gfCurrentTime + fOffset + frac(gfCurrentTime) * 1000.0f) % 256;
+	return(normalize(gRandomSphereBuffer.Load(u)));
+}
+
+void OutputParticleToStream(VS_PARTICLE_INPUT input, inout PointStream<VS_PARTICLE_INPUT> output)
+{
+	input.position += input.velocity * gfElapsedTime;
+	input.velocity += gf3Gravity * gfElapsedTime;
+	input.lifetime -= gfElapsedTime;
+
+	output.Append(input);
+}
+
+void EmmitParticles(VS_PARTICLE_INPUT input, inout PointStream<VS_PARTICLE_INPUT> output)
+{
+	float4 f4Random = RandomDirection(input.type);
+	if (input.lifetime <= 0.0f)
+	{
+		VS_PARTICLE_INPUT particle = input;
+
+		particle.type = PARTICLE_TYPE_SHELL;
+		particle.position = input.position + (input.velocity * gfElapsedTime * f4Random.xyz);
+		particle.velocity = input.velocity + (f4Random.xyz * 16.0f);
+		particle.lifetime = SHELL_PARTICLE_LIFETIME + (f4Random.y * 0.5f);
+
+		output.Append(particle);
+
+		input.lifetime = gfSecondsPerFirework * 0.2f + (f4Random.x * 0.4f);
+	}
+	else
+	{
+		input.lifetime -= gfElapsedTime;
+	}
+
+	output.Append(input);
+}
+
+void ShellParticles(VS_PARTICLE_INPUT input, inout PointStream<VS_PARTICLE_INPUT> output)
+{
+	if (input.lifetime <= 0.0f)
+	{
+		VS_PARTICLE_INPUT particle = input;
+		float4 f4Random = float4(0.0f, 0.0f, 0.0f, 0.0f);
+
+		particle.type = PARTICLE_TYPE_FLARE01;
+		particle.position = input.position + (input.velocity * gfElapsedTime * 2.0f);
+		particle.lifetime = FLARE01_PARTICLE_LIFETIME;
+
+		for (int i = 0; i < gnFlareParticlesToEmit; i++)
+		{
+			f4Random = RandomDirection(input.type + i);
+			particle.velocity = input.velocity + (f4Random.xyz * 18.0f);
+
+			output.Append(particle);
+		}
+
+		particle.type = PARTICLE_TYPE_FLARE02;
+		particle.position = input.position + (input.velocity * gfElapsedTime);
+		for (int j = 0; j < abs(f4Random.x) * gnMaxFlareType2Particles; j++)
+		{
+			f4Random = RandomDirection(input.type + j);
+			particle.velocity = input.velocity + (f4Random.xyz * 10.0f);
+			particle.lifetime = FLARE02_PARTICLE_LIFETIME + (f4Random.x * 0.4f);
+
+			output.Append(particle);
+		}
+	}
+	else
+	{
+		OutputParticleToStream(input, output);
+	}
+}
+
+void OutputEmberParticles(VS_PARTICLE_INPUT input, inout PointStream<VS_PARTICLE_INPUT> output)
+{
+	if (input.lifetime > 0.0f)
+	{
+		OutputParticleToStream(input, output);
+	}
+}
+
+void GenerateEmberParticles(VS_PARTICLE_INPUT input, inout PointStream<VS_PARTICLE_INPUT> output)
+{
+	if (input.lifetime <= 0.0f)
+	{
+		VS_PARTICLE_INPUT particle = input;
+
+		particle.type = PARTICLE_TYPE_FLARE03;
+		particle.position = input.position + (input.velocity * gfElapsedTime);
+		particle.lifetime = FLARE03_PARTICLE_LIFETIME;
+		for (int i = 0; i < 64; i++)
+		{
+			float4 f4Random = RandomDirectionOnSphere(input.type + i);
+			particle.velocity = input.velocity + (f4Random.xyz * 25.0f);
+
+			output.Append(particle);
+		}
+	}
+	else
+	{
+		OutputParticleToStream(input, output);
+	}
+}
+
+[maxvertexcount(128)]
+void GSParticleStreamOutput(point VS_PARTICLE_INPUT input[1], inout PointStream<VS_PARTICLE_INPUT> output)
+{
+	VS_PARTICLE_INPUT particle = input[0];
+
+	if (particle.type == PARTICLE_TYPE_EMITTER) EmmitParticles(particle, output);
+	else if (particle.type == PARTICLE_TYPE_SHELL) ShellParticles(particle, output);
+	else if ((particle.type == PARTICLE_TYPE_FLARE01) || (particle.type == PARTICLE_TYPE_FLARE03)) OutputEmberParticles(particle, output);
+	else if (particle.type == PARTICLE_TYPE_FLARE02) GenerateEmberParticles(particle, output);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+struct VS_PARTICLE_DRAW_OUTPUT
+{
+	float3 position : POSITION;
+	float4 color : COLOR;
+	float size : SCALE;
+	uint type : PARTICLETYPE;
+};
+
+struct GS_PARTICLE_DRAW_OUTPUT
+{
+	float4 position : SV_Position;
+	float4 color : COLOR;
+	float2 uv : TEXTURE;
+	uint type : PARTICLETYPE;
+};
+
+VS_PARTICLE_DRAW_OUTPUT VSParticleDraw(VS_PARTICLE_INPUT input)
+{
+	VS_PARTICLE_DRAW_OUTPUT output = (VS_PARTICLE_DRAW_OUTPUT)0;
+
+	output.position = input.position;
+	output.size = 2.5f;
+	output.type = input.type;
+
+	if (input.type == PARTICLE_TYPE_EMITTER) { output.color = float4(1.0f, 0.1f, 0.1f, 1.0f); output.size = 3.0f; }
+	else if (input.type == PARTICLE_TYPE_SHELL) { output.color = float4(0.1f, 0.0f, 1.0f, 1.0f); output.size = 3.0f; }
+	else if (input.type == PARTICLE_TYPE_FLARE01) { output.color = float4(1.0f, 1.0f, 0.1f, 1.0f); output.color *= (input.lifetime / FLARE01_PARTICLE_LIFETIME); }
+	else if (input.type == PARTICLE_TYPE_FLARE02) output.color = float4(1.0f, 0.1f, 1.0f, 1.0f);
+	else if (input.type == PARTICLE_TYPE_FLARE03) { output.color = float4(1.0f, 0.1f, 1.0f, 1.0f); output.color *= (input.lifetime / FLARE03_PARTICLE_LIFETIME); }
+
+	return(output);
+}
+
+static float3 gf3Positions[4] = { float3(-1.0f, +1.0f, 0.5f), float3(+1.0f, +1.0f, 0.5f), float3(-1.0f, -1.0f, 0.5f), float3(+1.0f, -1.0f, 0.5f) };
+static float2 gf2QuadUVs[4] = { float2(0.0f, 0.0f), float2(1.0f, 0.0f), float2(0.0f, 1.0f), float2(1.0f, 1.0f) };
+
+[maxvertexcount(4)]
+void GSParticleDraw(point VS_PARTICLE_DRAW_OUTPUT input[1], inout TriangleStream<GS_PARTICLE_DRAW_OUTPUT> outputStream)
+{
+	GS_PARTICLE_DRAW_OUTPUT output = (GS_PARTICLE_DRAW_OUTPUT)0;
+
+	output.type = input[0].type;
+	output.color = input[0].color;
+	for (int i = 0; i < 4; i++)
+	{
+		float3 positionW = mul(gf3Positions[i] * input[0].size, (float3x3)gmtxInverseView) + input[0].position;
+		output.position = mul(mul(float4(positionW, 1.0f), gmtxView), gmtxProjection);
+		output.uv = gf2QuadUVs[i];
+
+		outputStream.Append(output);
+	}
+	outputStream.RestartStrip();
+}
+
+float4 PSParticleDraw(GS_PARTICLE_DRAW_OUTPUT input) : SV_TARGET
+{
+	float4 cColor = gtxtParticleTexture.Sample(gWrapSamplerState, input.uv);
+	cColor *= input.color;
+
+	return(cColor);
+}
